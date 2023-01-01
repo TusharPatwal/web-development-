5559ffe32634bfacf4551f85ca91c30f471e9e0c
TusharPatwal <patwaltushar001@gmail.com>
2023-01-01 13:51:18 +0530

    web counter by js

---
+++ JavaScript/index01.js
@@ -0,0 +1,46 @@
+// document.getElementById("count-el").innerText = 5
+
+// let count = 0
+
+// console.log(count)
+
+// let sum = 5+4
+// console.log(sum)
+
+// let subtract = 5-4
+// console.log(subtract)
+
+// let multiply = 5*4
+// console.log(multiply)
+
+// let divide = 10/2
+// console.log(divide)
+
+// let myAge = 20
+// let humanDogRAtio = 7
+
+// let myDogAge = myAge * humanDogRAtio
+// console.log(myDogAge)
+
+
+
+// // reassigning the value and increment
+// let num = 5
+// num = 6
+// console.log(num)
+
+// num = num+1
+// console.log(num)
+
+
+// let bonusPoints = 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 50
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints - 75
+// console.log(bonusPoints)
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+
+<button id = "increment-btn"></button>
