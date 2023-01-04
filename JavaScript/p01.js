9e56e3f8f01ceed970885f6fa3e9383da87e10a7
TusharPatwal <patwaltushar001@gmail.com>
2023-01-04 19:24:27 +0530

    javaScript practice with strings vs. numbers

--- JavaScript/p01.js
+++ JavaScript/p01.js
@@ -1,3 +1,4 @@
+// strings vs. numbers in javaScript
 let name = 98
 let greeting = "Hi, my name is "
 let myGreeting = greeting + name
@@ -9,5 +10,10 @@ let bonusPoints = "10"
 
 let totalPoints = points + bonusPoints
 
-console.log(typeof(totalPoints))
-console.log(totalPoints)
+// console.log(typeof(totalPoints))
+// console.log(totalPoints)
+
+console.log(4+5)
+console.log("2"+"4")
+console.log("5"+1)
+console.log(100+"100")
