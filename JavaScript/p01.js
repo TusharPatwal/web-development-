3e0eb4d68c6965f2437579801a91072d5615ff1f
TusharPatwal <patwaltushar001@gmail.com>
2023-01-04 19:09:03 +0530

    strings vs. numbers in javaScript

--- JavaScript/p01.js
+++ JavaScript/p01.js
@@ -1,4 +1,13 @@
-let name = "Tushar"
+let name = 98
 let greeting = "Hi, my name is "
 let myGreeting = greeting + name
-console.log(myGreeting)
+// console.log(myGreeting)
+
+
+let points = 4 
+let bonusPoints = "10"
+
+let totalPoints = points + bonusPoints
+
+console.log(typeof(totalPoints))
+console.log(totalPoints)
