11013dd9ec790ec7823b1aca6498f16c492b8ca4
TusharPatwal <patwaltushar001@gmail.com>
2023-01-05 09:02:27 +0530

    printing previous count number

--- JavaScript/index01.js
+++ JavaScript/index01.js
@@ -2,10 +2,14 @@ let countEL = document.getElementById("count-el")
 console.log(countEL)
 let count = 0
 
+let saveEL = document.getElementById("save-el")
+
 function increment() {
-    count = count + 1
+    count += 1
     countEL.innerText = count
 }
 function save() {
+    let var1 = " " + count + " - "
+    saveEL.innerText += var1
     console.log(count)
 }
